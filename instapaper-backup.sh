76fd4ae5ec89a36c2ce794ccb26cbf3b13649af7
meddulla <meddulla.at@gmail.com>
2014-06-12 02:54:06 +0100

    fix path

--- instapaper-backup.sh
+++ instapaper-backup.sh
@@ -1,4 +1,4 @@
 export VIRTUAL_ENV="/home/pisofia/.virtualenvs/instaread"
 export PATH="$VIRTUAL_ENV/bin:$PATH"
-cd /srv/apps/instaread;
+cd /srv/apps/instapaper-backup;
 python instapaper-backup.py;
